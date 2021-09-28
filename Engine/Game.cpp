e3648bcace5ea1ce64a693815629a36c5065b667
raptorella <raptorella@@users.noreply.github.com>
2021-09-28 21:33:30 +0300

    beaauuutiful shapee

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -26,6 +26,17 @@ Game::Game( MainWindow& wnd )
 	wnd( wnd ),
 	gfx( wnd )
 {
+	for (int y = 0; y < surface.GetHeight(); y++)
+	{
+		for (int x = 0; x < surface.GetWidth(); x++)
+		{
+			surface.PutPixel(x, y, Color(
+				(x - 25) * (x - 25) + (y - 25) * (y - 25),
+				(x - 50) * (x - 50) + (y - 50) * (y - 50),
+				(x - 75) * (x - 75) + (y - 75) * (y - 75)
+				));
+		}
+	}
 }
 
 void Game::Go()
@@ -42,4 +53,5 @@ void Game::UpdateModel()
 
 void Game::ComposeFrame()
 {
+	gfx.DrawSprite(200, 200, surface);
 }
